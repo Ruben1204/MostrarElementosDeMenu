22ad8ea4b74c9abbb2cc5292d7b18aa4b9e0383e
Ruben Rueda <ingeniero.rubenrueda@gmail.com>
2023-12-22 17:19:40 -0500

    First Commit
    Example Menu in List

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ElementosDeMenu
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace ElementosDeMenu
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            listView.ItemsSource = ElementoTipoLista;
+        }
+
+        List<string> ElementoTipoLista = new List<string> 
+        {
+            "Elemento 1",
+            "Elemento 2",
+            "Elemento 3"
+        };
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem elemento = sender as MenuItem;
+
+            if(elemento?.BindingContext is string selectedItem)
+            {
+                DisplayAlert("Mensaje", $"Seleccionaste el elemento '{selectedItem}' de la lista", "Cancelar");
+            }
+        }
+    }
+
+}
